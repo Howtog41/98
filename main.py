4faac939dc2f94d62daccfca032021e956f58754
Howtog41 <howtog41@gmail.com>
2025-02-05 17:26:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,18 @@
 import os
 import importlib
-from pymongo import MongoClient
+
 from telebot import TeleBot
 
 # Replace with your bot token
 BOT_TOKEN = '8151017957:AAEhEOxbwjnw6Fxu1GzPwHTVhUeIpibpJqI'
 bot = TeleBot(BOT_TOKEN)
 
-# MongoDB connection setup
-MONGO_URI = "mongodb+srv://latestkoreandraama:UjebJR51Dki7Ili2@cluster0.nnnuejc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(MONGO_URI)
-db = client['mydatabase']  # Replace 'mydatabase' with your database name
-quizzes_collection = db['quizzes']  # Collection for storing quizzes
 
 # Load quizzes from MongoDB on startup
 saved_quizzes = {}  # To store all quizzes in memory
 creating_quizzes = {}  # Temporary in-memory storage for ongoing quizzes
 
-def fetch_quizzes():
-    """
-    Load all quizzes from MongoDB into the saved_quizzes dictionary.
-    """
-    for quiz in quizzes_collection.find():
-        saved_quizzes[quiz['quiz_id']] = quiz
-    print(f"Loaded {len(saved_quizzes)} quizzes from MongoDB.")
 
-def save_quiz_to_db(quiz_id, quiz_data):
-    """
-    Save or update a quiz in MongoDB.
-    """
-    quizzes_collection.update_one(
-        {"quiz_id": quiz_id},  # Filter
-        {"$set": quiz_data},  # Update data
-        upsert=True  # Insert if not exists
-    )
 
 # Dynamically load plugins
 def load_plugins():
