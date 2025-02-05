c36ff1cfc25581ab25d84075ed06a15e38ba4091
Howtog41 <howtog41@gmail.com>
2025-02-05 16:57:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import os
 import importlib
 from telebot import TeleBot
-from pymongo import MongoClient
-# Replace with your bot token
-BOT_TOKEN = '8151017957:AAEhEOxbwjnw6Fxu1GzPwHTVhUeIpibpJqI'
-bot = TeleBot(BOT_TOKEN)
-MONGO_URI = "mongodb+srv://latestkoreandraama:UjebJR51Dki7Ili2@cluster0.nnnuejc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(MONGO_URI)
-db = client['quiz_bot']
 
-saved_quizzes = db['quizzes'] # To store all quizzes
-creating_quizzes = db['sessions']
+# Replace with your bot token
+BOT_TOKEN = '7961958205:AAHX-UoP5dHLUJLKOM_QtW0ayENLoxNguHI'
+bot = TeleBot(BOT_TOKEN)
+
+saved_quizzes = {}  # To store all quizzes
+creating_quizzes = {}
 
 # Dynamically load plugins
 def load_plugins():
