988dcda04e55b6220660b81e24f22fa56fe6d64e
Howtog41 <howtog41@gmail.com>
2025-02-09 10:49:42 +0530

    Update start_quiz.py

--- plugins/start_quiz.py
+++ plugins/start_quiz.py
@@ -163,8 +163,7 @@ def register_handlers(bot, saved_quizzes, creating_quizzes, save_quiz_to_db):
             quiz_data["current_question_index"] += 1
 
             
-            send_question(bot, quiz_data["chat_id"], quiz_id, quiz_data["current_question_index"])
-       
+            
         next_question_index = quiz_data["current_question_index"]
 
     # Send the next question or finalize the quiz if completed
