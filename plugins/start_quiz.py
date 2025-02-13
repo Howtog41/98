02b0e31d61a9b45437b184fec10e01d9e06c160a
Howtog41 <howtog41@gmail.com>
2025-02-13 09:29:11 +0530

    Update start_quiz.py

--- plugins/start_quiz.py
+++ plugins/start_quiz.py
@@ -2,6 +2,7 @@ import threading
 import time
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+quizzes_collection = db['quizzes']
 active_quizzes = {}
 lock = threading.Lock()  # Thread-safe lock for active_quizzes
 saved_quizzes = {}
